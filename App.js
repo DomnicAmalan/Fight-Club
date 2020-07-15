e43fbe4d7b4ecaf818c48aeafdb621f648518ed6
Domnic <amalandomnic@gmail.com>
2020-07-15 09:34:32 +0530

    social auth ongoing

--- App.js
+++ App.js
@@ -5,73 +5,27 @@
  * @format
  * @flow strict-local
  */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'react-native-gesture-handler'
 
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-import SignInScreen from './components/AuthComponent'
-
-
+import AuthRoute from './components/AuthComponent'
+import AppComponent from './components/AppComponent'
+import AsyncStorage from '@react-native-community/async-storage';
 
 const App: () => React$Node = () => {
+
+  const [isLoggedIn, setLogin] = useState(false);
+  const [logginType, setLoginType] = useState(null);
+  const [identifier, setIdentifier] = useState(null);
+
   return (
-    <SignInScreen />
+    <>
+      {
+        isLoggedIn ?  <AppComponent /> : <AuthRoute />
+      }
+    </>
+    
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
--- components/AuthComponent.js
+++ components/AuthComponent.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Image, Platform } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Image, Platform, Dimensions, TextInput, Button, PickerIOSItem } from 'react-native';
 import auth from '@react-native-firebase/auth';
 import { GoogleSignin } from '@react-native-community/google-signin';
 import appleAuth, { AppleAuthRequestScope, AppleAuthRequestOperation } from '@invertase/react-native-apple-authentication';
 import { LoginManager, AccessToken } from 'react-native-fbsdk';
+import { NavigationContainer, CommonActions } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import {Picker} from '@react-native-community/picker';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import countries from '../constants/countries'
+
+const DashboardStack = createStackNavigator();
 
 
 const GOOGLE_CLIENT_ID = "736979816766-t7pljfne534v2o29v7d0hpbkg57tesdc.apps.googleusercontent.com"
@@ -12,7 +19,7 @@ GoogleSignin.configure({
 });
 
 const SignInTabs = (props, {navigation}) => {
-    let resp = null
+    let resp = null;
     const CallSignInMethod = async(type) => {
         if(type == "GOOGLE"){
             resp = await GoogleButtonPress()
@@ -23,11 +30,8 @@ const SignInTabs = (props, {navigation}) => {
         else if(type == "FACEBOOK"){
             resp = await FacebookButtonPress()
         }
-        else if(type == "MOBILE"){
-            return <Phone />
-        }
     }
-    console.log(resp)
+    console.log(resp, "yes")
     
     return(
         <TouchableOpacity style={tabStyle.tab} onPress={() => CallSignInMethod(props.navigation)}>
@@ -38,12 +42,6 @@ const SignInTabs = (props, {navigation}) => {
                 <Text style={tabStyle.title}>{props.title}</Text>
             </View>
         </TouchableOpacity>
-    )
-}
-
-const Phone = () => {
-    return(
-        <View><Text>Test</Text></View>
     )
 }
 
@@ -65,12 +63,9 @@ const AppleButtonPress = async() => {
   
     // Sign the user in with the credential
     const resp =  auth().signInWithCredential(appleCredential);
-    console.log(resp)
-    return resp
 }
 
 const GoogleButtonPress = async() => {
-    console.log("yes")
     // Get the users ID token
     const { idToken } = await GoogleSignin.signIn();
   
@@ -79,8 +74,6 @@ const GoogleButtonPress = async() => {
   
     // Sign-in the user with the credential
     const resp =  await auth().signInWithCredential(googleCredential);
-    console.log(resp)
-    return resp
 }
 
 
@@ -103,24 +96,109 @@ async function FacebookButtonPress() {
     const facebookCredential = auth.FacebookAuthProvider.credential(data.accessToken);
   
     // Sign-in the user with the credential
-    const resp =  auth().signInWithCredential(facebookCredential);
-    console.log(resp)
-    return resp
+    const resp =  await auth().signInWithCredential(facebookCredential);
 }
 
-const SignInScreen = () => {
+const SignInScreen = ({navigation}) => {
     return(
         <View style={tabStyle.container}>
             <SignInTabs image={require('../assets/auth-logos/google-logo.png')} title={"CONTINUE WITH GOOGLE"} navigation={"GOOGLE"}/>
             <SignInTabs image={require('../assets/auth-logos/facebook.png')} title={"CONTINUE WITH FACEBOOK"} navigation={"FACEBOOK"}/>
             { Platform.OS =="ios" ? <SignInTabs image={require('../assets/auth-logos/apple.png')} title={"CONTINUE WITH APPLE"} navigation={"APPLE"} /> : null }
             <View><Text style={tabStyle.title}> - OR </Text></View>
-            <SignInTabs image={require('../assets/auth-logos/mobile.png')} title={"CONTINUE WITH MOBILE"} />
+            <TouchableOpacity style={tabStyle.tab} onPress={() => navigation.navigate('Phone') }>
+                <View style={tabStyle.imageView}>
+                    <Image source={require('../assets/auth-logos/mobile.png')} title={"CONTINUE WITH MOBILE"} style={tabStyle.image}></Image>
+                </View>
+                <View style={tabStyle.titleView}>
+                    <Text style={tabStyle.title}>CONTINUE WITH MOBILE</Text>
+                </View>
+            </TouchableOpacity>
         </View>
     )
 }
 
+const  PhoneAuthScreen = () => {
+  // If null, no SMS has been sent
+    const [confirm, setConfirm] = useState(null)
+    const [mobileNumber, setMobileNumber] = useState(null)
+    const [code, setCode] = useState(null)
+    const [mobileNumberError, setMobileNumberError] = useState(null)
+    const [countryCode, setCountryCode] = useState(null)
 
+  // Handle the button press
+    async function signInWithPhoneNumber() {
+        if(mobileNumber){
+            if(!countryCode){
+                setMobileNumberError("Please select your country code")
+                setTimeout(() => {
+                    setMobileNumberError(null);
+                }, 3000)
+            }
+            else{
+                let number = countryCode + mobileNumber
+                const confirmation = await auth().signInWithPhoneNumber(number);
+                setConfirm(confirmation)
+            }
+        }
+        else{
+            setMobileNumberError("Please Enter Mobile Number")
+            setTimeout(() => {
+                setMobileNumberError(null);
+            }, 3000)
+        }
+    }
+
+    const VerifyCode = async() => {
+        try {
+          const resp = await confirm.confirm(code);
+          console.log(resp, "yes")
+          
+          return true
+        } catch (error) {
+          console.log('Invalid code.');
+        }
+    }
+
+    const countryValues = () => {
+        let country = []
+        countries.forEach((ele) => {
+            country.push(
+                <Picker.Item key={ele["code"]} label={ele["flag"] + " " + ele["name"]} value={ele["dial_code"]} />
+            )
+        })
+        return country
+    }
+    
+    return (
+        <View style={tabStyle.confirm}>
+            <View style={tabStyle.Number}>
+                <Picker
+                    selectedValue={countryCode}
+                    onValueChange={(itemValue, itemIndex) => setCountryCode(itemValue)}
+                    style={tabStyle.picker}
+                >
+                    {countryValues()}
+                </Picker>
+                <TextInput
+                    style={tabStyle.MobileNumberInput}
+                    title="Phone Number Sign In"
+                    onChangeText={(text)=>setMobileNumber(text)}
+                    value={mobileNumber}
+                    keyboardType={'phone-pad'}
+                    editable={confirm ? false : true} selectTextOnFocus={confirm ? false: true} 
+                />
+                {mobileNumberError ? <View><Text style={tabStyle.error}>{mobileNumberError}</Text></View>: <View />}
+                <TouchableOpacity  onPress={() =>  signInWithPhoneNumber()}><Text>Send</Text></TouchableOpacity>
+            </View>
+            {confirm ? 
+            <View>
+                <TextInput value={code} onChangeText={text => setCode(text)} />
+                <Button title="Confirm Code" onPress={() => VerifyCode()} />
+            </View> : null}
+        </View>
+    )
+}
 
 
 const tabStyle = StyleSheet.create({
@@ -159,8 +237,48 @@ const tabStyle = StyleSheet.create({
     titleView:{
         alignSelf: "center",
         paddingHorizontal: 10,
+    },
+    confirm: {
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    MobileNumberInput: {
+        elevation: 1,
+        width: 250,
+        height: 60,
+        textAlign: "center",
+        flexDirection: "row",
+    },
+    Number: {
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    countryCode: {
+
+    },
+    error: {
+        color: "red",
+        fontFamily: "helvetica",
+        fontSize: 7
+    },
+    picker: {
+        height: 50,
+        width: 300,
+        
     }
 })
 
+const AuthRoute = () => {
+    return(
+        <NavigationContainer>
+            <DashboardStack.Navigator mode='modal' headerMode='none'>
+                <DashboardStack.Screen name="MainAuth" component={SignInScreen} />
+                <DashboardStack.Screen name="Phone" component={PhoneAuthScreen} />
+            </DashboardStack.Navigator>
+        </NavigationContainer>
+    )
+}
 
-export default SignInScreen;
+
+export default AuthRoute;
